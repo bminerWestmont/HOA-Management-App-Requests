dbb82a0c4123ce52736ea26f20a96a2e2a65a327
tstoleson <tstoleson@westmont.edu>
2019-11-23 14:15:01 -0800

    created requests page for home owner

---
+++ home-owner/requests/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+
+    <title>HOA Home-Owner Page</title>
+
+    <script type="text/javascript" src="../../node_modules/jquery/dist/jquery.min.js"></script>
+    <script type="text/javascript" src="../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script type="text/javascript" src="./js/script.js"></script>
+
+    <link rel="stylesheet" type="text/css" href="../../node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="./css/styles.css">
+  </head>
+<body>
+
+<h1>New Request</h1>
+
+
+<h2>Upload Photos</h2>
+
+<button type="button" onclick="alert('broken link')">(Download Image)</button>
+ 
+</body>
+</html>
