d25c7e2ddfd57b15224d87b45931cce1f717542d
cameron-franklin <cfranklin@westmont.edu>
2019-11-23 14:05:39 -0800

    basic landing pages for homeowner and manager

--- home-owner/index.php
+++ home-owner/index.php
@@ -7,18 +7,44 @@
 
     <title>HOA Home-Owner Page</title>
 
-    <script type="text/javascript" src="./node_modules/jquery/dist/jquery.min.js"></script>
-    <script type="text/javascript" src="./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script type="text/javascript" src="../node_modules/jquery/dist/jquery.min.js"></script>
+    <script type="text/javascript" src="../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
     <script type="text/javascript" src="./js/script.js"></script>
 
-    <link rel="stylesheet" type="text/css" href="./node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../node_modules/bootstrap/dist/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="./css/styles.css">
   </head>
   <!-- //accent color #f2cf33 -->
   <!-- //main color  #7dc9b5 -->
-  <body>
+<body>
 
-<h1>HOA Home-Owner Page</h1>
+  <div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">HO Landing Page</h1>
+    <p class="lead">This is the landing page for a homwowner in our HOA app</p>
+  </div>
+</div>
+<div class="container">
+
+  <div class="row">
+    <button class="col btn btn-lg bg-primary"> content
+    </button>
+  </div>
+   
+   <div class="row">
+    <button class="col btn btn-lg border-top bg-primary">content</button>
+  </div>
+  
+  <div class="row">
+    <div class="col">.</div>
+  </div>
+  
+  <div class="row">
+    <button class="col-sm btn btn-lg bg-primary">received requests</button>
+    <div class="col-sm-1"></div>
+    <button class="col-sm btn btn-lg bg-primary border">new request</button>
+  </div>
+</div>
 
 </body>
 </html>
