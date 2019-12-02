4307be6075831c2514e3f590184a4e2959a35528
cameron-franklin <cfranklin@westmont.edu>
2019-12-01 20:13:25 -0800

    changed origin file for localhost to the login page

---
+++ home-owner/New folder/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+
+    <title>HOA Home-Owner Contact</title>
+
+    <script type="text/javascript" src="../../node_modules/jquery/dist/jquery.min.js"></script>
+    <script type="text/javascript" src="../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script type="text/javascript" src="./js/script.js"></script>
+
+    <link rel="stylesheet" type="text/css" href="../../node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="./css/styles.css">
+  </head>
+  <!-- //accent color #f2cf33 -->
+  <!-- //main color  #7dc9b5 -->
+  <body>
+
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">  
+    <img src="/docs/4.3/assets/brand/bootstrap-solid.svg" width="30" height="30" alt=""> <!-- What is this image trying to point to? this url does not exist in this directory... -->
+  </a></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link" href="#">HOME <span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link" href="../requests/index.php">NEW REQUEST</a>
+      <a class="nav-item nav-link" href="../ReceivedRequests/index.php">RECEIVED REQUESTS</a>
+      <a class="nav-item nav-link" href="#">BUILDING MAINTENANCE</a>
+      <a class="nav-item nav-link" href="#">CALENDAR</a>
+      <a class="nav-item nav-link active" href="./index.php">CONTACT</a>
+     
+    </div>
+  </div>
+</nav>
+
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4">Contact</h1>
+        <p class="lead">This is the contact page</p>
+      </div>
+    </div>
+    <div class="container">
