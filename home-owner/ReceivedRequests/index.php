a624c9f62a23444fe4c647381d467cb2a6f8d0db
cameron-franklin <cfranklin@westmont.edu>
2019-12-02 17:45:35 -0800

    these pages are now properly pulling the css and js files

--- home-owner/ReceivedRequests/index.php
+++ home-owner/ReceivedRequests/index.php
@@ -9,11 +9,11 @@
 
     <script type="text/javascript" src="../../node_modules/jquery/dist/jquery.min.js"></script>
     <script type="text/javascript" src="../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-    <script type="text/javascript" src="./js/script.js"></script>
+    <script type="text/javascript" src="../js/script.js"></script>
 
     <link rel="stylesheet" type="text/css" href="../../node_modules/bootstrap/dist/css/bootstrap.min.css">
     <!-- <link rel="stylesheet" type="text/css" href="../node_modules/@fortawesome/fontawesome-free/css/all.css"> -->
-    <link rel="stylesheet" type="text/css" href="./css/styles.css">
+    <link rel="stylesheet" type="text/css" href="../css/styles.css">
   </head>
   <body>
 
