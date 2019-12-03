a78e7fac552d55e4e77919f8f2e21a3daf065758
“Jordan <jdouthit@westmont.edu>
2019-12-02 19:34:50 -0800

    added path for php navBar

--- home-owner/requests/index.php
+++ home-owner/requests/index.php
@@ -17,33 +17,9 @@
 <body>
 
 
-
-
-
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">  
-    <img src="/docs/4.3/assets/brand/bootstrap-solid.svg" width="30" height="30" alt=""> <!-- What is this image trying to point to? this url does not exist in this directory... -->
-  </a></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div class="navbar-nav">
-      <a class="nav-item nav-link" href="../index.php">HOME <span class="sr-only">(current)</span></a>
-      <a class="nav-item nav-link active" href="#">NEW REQUEST</a>
-      <a class="nav-item nav-link" href="../ReceivedRequests/index.php">RECEIVED REQUESTS</a>
-      <a class="nav-item nav-link" href="#">BUILDING MAINTENANCE</a>
-      <a class="nav-item nav-link" href="#">CALENDAR</a>
-      <a class="nav-item nav-link" href="../contact/index.php">CONTACT</a>
-     
-    </div>
-  </div>
-</nav>
-
-
-
-
-
+<?php
+include './navBar.php';
+?>
 
 <div class="jumbotron jumbotron-fluid">
         <div class="container">
