c5ed06802cc6fb9c9e5023093e74f2114da8d505
“Jordan <jdouthit@westmont.edu>
2019-12-03 09:35:14 -0800

    fixed paths so they go to the right thing now

--- home-owner/confirmedRequest.php
+++ home-owner/confirmedRequest.php
@@ -24,20 +24,20 @@
 <body>
 <?php
   $image_path = "../abs/media/logo.jpg";
-  print_home_owner_navbar($path);
+  print_home_owner_navbar($image_path, $current_url);
 ?>
 
-<div class="jumbotron jumbotron-fluid">
+<div class="jumbotron jumbotron-fluid jumboBG">
     <div class="container">
-        <h1 class="display-4">Home Owner New Request</h1>
-        <p class="lead">If you have any maintenance requests, fixes, or general concerns about the building safety, please fill out this request form.</p>
+        <h1 class="display-4">We have received your request!</h1>
+        <p class="lead"> !!!  </p>
     </div>
 </div>
 
 <h1> Request Recieved! Management will get right on it and notify you when we have begun working on your request. </h1>
 
 <<div class="row">
-        <a  href="../index.php" class="col-sm btn btn-lg bg-primary" href="./ReceivedRequests/index.php">Go Home</a>
+        <a  href="../landing.php" class="col-sm btn btn-lg bg-primary" href="./receivedRequests.php">Go Home</a>
 <div class="col-sm-1"></div>
 
 
--- home-owner/newRequest.php
+++ home-owner/newRequest.php
@@ -1,136 +1,139 @@
 <?php
-  require('../abs/navBar.php');
-  $filename = "./" . basename(__FILE__);
+require('../abs/navBar.php');
+$filename = "./" . basename(__FILE__);
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 
-    <title>HOA Home-Owner Page</title>
+  <title>HOA HomeOwner Page</title>
 
-    <script type="text/javascript" src="../node_modules/jquery/dist/jquery.min.js"></script>
-    <script type="text/javascript" src="../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-    <script type="text/javascript" src="./js/script.js"></script>
+  <script type="text/javascript" src="../node_modules/jquery/dist/jquery.min.js"></script>
+  <script type="text/javascript" src="../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+  <script type="text/javascript" src="./js/script.js"></script>
 
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,800&display=swap" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="../node_modules/bootstrap/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="./css/styles.css">
-  </head>
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,800&display=swap" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="../node_modules/bootstrap/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="./css/styles.css">
+</head>
 <body>
-<?php
+  <?php
   $image_path = "../abs/media/logo.jpg";
   print_home_owner_navbar($image_path, $filename);
-?>
+  ?>
 
-<div class="jumbotron jumbotron-fluid">
-        <div class="container">
-            <h1 class="display-4">HO New Request</h1>
-            <p class="lead">This is where to make a new request</p>
-        </div>
+  <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+      <h1 class="display-4">New Request</h1>
+      <p class="lead">If you have any maintenance requests, fixes, or general concerns about the building safety, please fill out this request form.</p>
+    </div>
+  </div>
+
+
+
+
+
+  <h1> New Request </h1>
+
+
+
+
+  <div class="container">
+
+    <div class="row">
+
+
+      <div class="form-group">
+        <label for="exampleFormControlSelect1">Type:</label>
+        <select class="form-control" id="exampleFormControlSelect1">
+          <option>Plumbing</option>
+          <option>Electrical</option>
+          <option>Treescaping</option>
+          <option>Wall / ground repair</option>
+          <option>Pest control</option>
+        </select>
+      </div>
+
+
     </div>
 
 
 
 
 
-<h1> New Request </h1>
+
+    <div class="form-group">
+      <label for="exampleFormControlTextarea1">Description</label>
+      <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+    </div>
 
 
 
 
-<div class="container">
+
+    <div class="container">
 
       <div class="row">
-         
 
-<div class="form-group">
-    <label for="exampleFormControlSelect1">Type:</label>
-    <select class="form-control" id="exampleFormControlSelect1">
-      <option>Plumbing</option>
-      <option>Electrical</option>
-      <option>Treescaping</option>
-      <option>Wall / ground repair</option>
-      <option>Pest control</option>
-    </select>
-  </div>
-  
 
+        <div class="form-group">
+          <label for="exampleFormControlSelect1">Where:</label>
+          <select class="form-control" id="exampleFormControlSelect1">
+            <option>In my appartment</option>
+            <option>In the hall</option>
+            <option>By the pool</option>
+            <option>Front entryway</option>
+            <option>Other</option>
+          </select>
+        </div>
+
+
+      </div>
+
+    </div>
+
+
+
+
+
+    <div class="container">
+
+      <div class="row">
+
+
+       <div class="form-group">
+        <label for="exampleFormControlSelect1">Urgency:</label>
+        <select class="form-control" id="exampleFormControlSelect1">
+          <option>Very urgent</option>
+          <option>Somewhat urgent</option>
+          <option>Not very urgent</option>
+        </select>
       </div>
 
 
 
+      <form>
+        <div class="form-group">
+          <label for="exampleFormControlFile1">Upload Images</label>
+          <input type="file" class="form-control-file" id="exampleFormControlFile1">
+        </div>
+      </form>
 
 
-
-<div class="form-group">
-    <label for="exampleFormControlTextarea1">Description</label>
-    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-  </div>
- 
-
-
-
-
-<div class="container">
-
-      <div class="row">
-         
-
-<div class="form-group">
-    <label for="exampleFormControlSelect1">Where:</label>
-    <select class="form-control" id="exampleFormControlSelect1">
-      <option>In my appartment</option>
-      <option>In the hall</option>
-    </select>
-  </div>
-
+    </div>
 
   </div>
 
-      </div>
 
 
 
+  <button type="button" onclick="window.location.href = '../confirmedRequest.php';" >Submit</button>
 
-
-<div class="container">
-
-      <div class="row">
-         
-
- <div class="form-group">
-    <label for="exampleFormControlSelect1">Urgency:</label>
-    <select class="form-control" id="exampleFormControlSelect1">
-      <option>Very urgent</option>
-      <option>Somewhat urgent</option>
-      <option>Not very urgent</option>
-    </select>
-  </div>
-
-
-
-<form>
-  <div class="form-group">
-    <label for="exampleFormControlFile1">Upload Images</label>
-    <input type="file" class="form-control-file" id="exampleFormControlFile1">
-  </div>
-</form>
-
-
-  </div>
-  
-      </div>
-
-
-
-
-<button type="button" onclick="window.location.href = '../ConfirmedRequest/index.php';" >Submit</button>
-        
 
 
 
