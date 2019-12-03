5054eca4aaff9ec5759e22bd3a307e5dbc065303
Bryan Miner <gymbrychamp@gmail.com>
2019-12-02 21:37:50 -0800

    got navbar fully working

--- manager/landing.php
+++ manager/landing.php
@@ -1,5 +1,6 @@
 <?php
   require('../abs/navBar.php');
+  $filename = "./" . basename(__FILE__);
 ?>
 
 <!DOCTYPE html>
@@ -22,8 +23,8 @@
 <!-- //main color  #7dc9b5 -->
 <body>
 <?php
-  $path = "../abs/media/logo.jpg";
-  print_manager_navbar($path);
+  $image_path = "../abs/media/logo.jpg";
+  print_manager_navbar($image_path, $filename);
 ?>
 	<div class="jumbotron jumbotron-fluid">
 		<div class="container">
--- manager/vendorAssign.php
+++ manager/vendorAssign.php
@@ -1,5 +1,6 @@
 <?php
   require('../abs/navBar.php');
+  $filename = "./" . basename(__FILE__);
 ?>
 
 <!DOCTYPE html>
@@ -17,23 +18,13 @@
 
   <link rel="stylesheet" type="text/css" href="../node_modules/bootstrap/dist/css/bootstrap.min.css">
   <link rel="stylesheet" type="text/css" href="../css/styles.css">
-
-  <style type="text/css">
-
-    .top-buffer{margin-top: 20px;}
-
-    .row{height: 60px;}
-
-    .headerFont{font-size: 30px;}
-
-  </style>
 </head>
 <!-- //accent color #f2cf33 -->
 <!-- //main color  #7dc9b5 -->
 <body>
 <?php
-  $path = "../abs/media/logo.jpg";
-  print_manager_navbar($path);
+  $image_path = "../abs/media/logo.jpg";
+  print_manager_navbar($image_path, $filename);
 ?>
 
   <div class="jumbotron jumbotron-fluid">
