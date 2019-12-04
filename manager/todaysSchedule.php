c1fcc66a4f2be9408fe426097ea3c45b4ad4725b
tstoleson <tstoleson@westmont.edu>
2019-12-03 17:01:51 -0800

    Built close spec as provided in invision, buttons do not link anywhere, and can not re-arrange or search

--- manager/todaysSchedule.php
+++ manager/todaysSchedule.php
@@ -31,7 +31,86 @@
   <div class="jumbotron jumbotron-fluid">
     <div class="container">
       <h1 class="display-4">Today's Schedule </h1>
-      <p class="lead">Everything going on in the building today. </p>
+      <p class="lead">Here's everything going on in the building today.</p>
     </div>
   </div>
+
+
   <div class="container">
+    
+
+    <div class="row">
+     <div class="col-sm-8">
+      <input type="text" placeholder="Search..">
+     </div>
+     <div class="col-sm-4">
+      <button type="submit">Submit</button>
+    </div>  
+   </div>
+
+   <br>
+
+   <div class="row">
+     <div class="col-sm-6">
+       <div class="form-group">
+          <select class="form-control" id="exampleFormControlSelect1">
+            <option>Today</option>
+            <option>option 2</option>
+            <option>option 3</option>
+            <option>option 4</option>
+            <option>option 5</option>
+          </select>
+        </div>
+     </div>
+     <div class="col-sm-6">
+       <div class="form-group">
+          <select class="form-control" id="exampleFormControlSelect1">
+            <option>In Progress</option>
+            <option>option 2</option>
+            <option>option 3</option>
+            <option>option 4</option>
+            <option>option 5</option>
+          </select>
+        </div>
+     </div>
+   </div>
+
+    <div class="row">
+      <div class="col border jumboBG text-center">ALL RECEIVED REQUESTS</div>
+    </div>
+
+    <div class="row">
+     <div class="col-sm-2 border jumboBG">Ticket Number</div>
+     <div class="col-sm-2 border jumboBG">Title</div>
+     <div class="col-sm-2 border jumboBG">Resident</div>
+     <div class="col-sm-2 border jumboBG">Date Requested</div>  
+     <div class="col-sm-2 border jumboBG">Location</div>
+     <div class="col-sm-2 border jumboBG">Status</div>  
+   </div>
+   <div class="row">
+     <div class="col-sm-2 border">69422</div>
+     <div class="col-sm-2 border">Light Fixture Fix</div>
+     <div class="col-sm-2 border">Ellen Q.</div>
+     <div class="col-sm-2 border">12/1/19</div>  
+     <div class="col-sm-2 border">B322</div>
+     <div class="col-sm-2 border">Doing work today</div>  
+   </div>
+   <div class="row">
+     <div class="col-sm-2 border">77712</div>
+     <div class="col-sm-2 border">Plummer fixing pipes</div>
+     <div class="col-sm-2 border">Mark Q.</div>
+     <div class="col-sm-2 border">12/2/19</div>  
+     <div class="col-sm-2 border">General Building</div>
+     <div class="col-sm-2 border">Doing Work Today</div>  
+   </div>
+   <div class="row">
+     <div class="col-sm-2 border">10076</div>
+     <div class="col-sm-2 border">Fix Outside Gate</div>
+     <div class="col-sm-2 border">HOA Board</div>
+     <div class="col-sm-2 border">11/20/19</div>  
+     <div class="col-sm-2 border">Back Entrance</div>
+     <div class="col-sm-2 border">Finding Operators</div>  
+   </div>
+
+
+ </div>
